d53b3be3b096fa060f6caf9e025719d6e07a4c90
dal <dal@1902.software>
2021-11-03 21:56:44 +0800

    initcommit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { FavoritesContextProvider } from './store/favorites-context';
+
+ReactDOM.render(
+    <FavoritesContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavoritesContextProvider>,
+    document.getElementById('root')
+);
