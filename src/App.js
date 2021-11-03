29ede29d08fe3845b95b430c73efa9ca4677cae2
dal <dal@1902.software>
2021-11-03 21:58:26 +0800

    initcommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-
+import Todo from './components/Todo';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return <div>
+        <h1>My Todos</h1>
+        <Todo text='Learn React' />
+        <Todo text='Master React' />
+        <Todo text='Explore React' />
+    </div>;
 }
 
 export default App;
