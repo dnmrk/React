ce081207ff765bcfd32dcd34de7d35b9a55a96b5
dal <dal@1902.software>
2021-12-06 20:41:31 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
+  const showCart = useSelector(state => state.ui.cartIsVisible);
   return (
-    <div>
-
-    </div>
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
   );
 }
 
