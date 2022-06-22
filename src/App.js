5dda3e2b952e9d081ccdb43f8a04b1a416efe388
dal <dal@1902.software>
2022-06-22 22:30:22 +0800

    - react authentication/firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
-  return (
-    <div>
+  const authCtx = useContext(AuthContext);
 
-    </div>
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        {!authCtx.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        )}
+          <Route path='/profile'>
+            {authCtx.isLoggedIn && <UserProfile />}
+            {!authCtx.isLoggedIn && <Redirect to='/auth' />}
+          </Route>
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
