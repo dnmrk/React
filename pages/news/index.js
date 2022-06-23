68f5db432b6d010e9e43817f65f981ea762477f5
dal <dal@1902.software>
2022-06-23 22:41:08 +0800

    - setup, pages, Link

---
+++ pages/news/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import { Fragment } from "react";
+
+function NewsPage() {
+    return (<Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li><Link href="/news/nextjs-is-a-great-framework">NextJS is a great framework!</Link></li>
+            <li><Link href="/news/something-else">Something else</Link></li>
+        </ul>
+    </Fragment>);
+}
+
+export default NewsPage;
