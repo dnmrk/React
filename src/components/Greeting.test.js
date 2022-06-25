33af3572888e30da0888e8a33f5a939fab1786e8
dal <dal@1902.software>
2022-06-26 00:06:18 +0800

    - group tests

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Hello World as a text', () => {
-    //arrange
-    render(<Greeting />);
+describe('Greeting Component', () => {
+    test('renders Hello World as a text', () => {
+        //arrange
+        render(<Greeting />);
 
-    //act
-    //... nothing
+        //act
+        //... nothing
 
-    //assert
-    const helloWordElement = screen.getByText('Hello World!', {
-        exact: true
+        //assert
+        const helloWordElement = screen.getByText('Hello World!', {
+            exact: true
+        });
+
+        expect(helloWordElement).toBeInTheDocument();
     });
 
-    expect(helloWordElement).toBeInTheDocument();
 });
+
