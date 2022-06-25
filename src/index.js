4c9a43e828e015b3928073d19fee86e2081c3bc6
dal <dal@1902.software>
2022-06-25 21:47:24 +0800

    - replace redux to react hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import ProductsProvider from './context/products-context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById('root')
+);
