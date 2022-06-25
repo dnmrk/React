d87ca16c6808ef31480931580d4eae1eef056283
dal <dal@1902.software>
2022-06-26 00:21:59 +0800

    test with arrange,action,assert

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting Component', () => {
@@ -16,6 +17,54 @@ describe('Greeting Component', () => {
 
         expect(helloWordElement).toBeInTheDocument();
     });
+
+    test('renders good to se you if the button was NOT Clicked', () => {
+        //arrange
+        render(<Greeting />);
+
+        //act
+        //... nothing
+
+        //assert
+        const helloWordElement = screen.getByText('good to see you!', {
+            exact: false
+        });
+
+        expect(helloWordElement).toBeInTheDocument();
+    });
+
+    test('renders Changed if the button was Clicked', () => {
+        //arrange
+        render(<Greeting />);
+
+        //act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        //assert
+        const outputElement = screen.getByText('Changed!', {
+            exact: true
+        });
+
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('does not renders good to see you if the button was Clicked', () => {
+        //arrange
+        render(<Greeting />);
+
+        //act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        //assert
+        const outputElement = screen.queryByText('good to see you', {
+            exact: false
+        });
+
+        expect(outputElement).toBeNull();
+    });
+
 
 });
 
