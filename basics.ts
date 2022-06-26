6bf0e084bfaa09d426fb4dc38c17030fa6142f38
dal <dal@1902.software>
2022-06-26 15:43:27 +0800

    typescript primitive

---
+++ basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// function types, parameters
+
+//Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
