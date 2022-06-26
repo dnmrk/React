d3199051a0bfccfa6156b7d391d99592c9560f91
dal <dal@1902.software>
2022-06-26 16:10:28 +0800

    Generics TypeSave yet flexible

--- basics.ts
+++ basics.ts
@@ -56,3 +56,19 @@ function print(value: any) {
   console.log(value);
   //return type void
 }
+
+// Generics TypeSave yet flexible
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+updatedArray[0].split(""); 
+
+stringArray[0].split("");
