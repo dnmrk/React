81cc326a49bb89e6433dd380dcc87e6df0bd68a8
dal <dal@1902.software>
2022-06-26 23:08:40 +0800

    - type anotation, working with props & typescript

--- react-ts/src/components/Todo.tsx
+++ react-ts/src/components/Todo.tsx
@@ -1,7 +1,9 @@
-const Todos: React.FC = (props) => {
+const Todos: React.FC<{ items: string[] }> = (props) => {
     return (
         <ul>
-            {props.children}
+            {props.items.map(item => (
+                <li key={item}>{item}</li>
+            ))}
         </ul>
     );
 }
